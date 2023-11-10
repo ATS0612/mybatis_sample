a8cf6d66e9d5d7703a85c4668462db7cda6bca74
Atsushi Sakaguchi <ats.pantomime612@gmail.com>
2023-11-11 03:38:57 +0900

    firstCommit(lesson02課題着手状態)

--- src/main/java/com/example/mapper/CourseMapper.java
+++ src/main/java/com/example/mapper/CourseMapper.java
@@ -3,10 +3,25 @@ package com.example.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import com.example.entity.Course;
 
 @Mapper
 public interface CourseMapper {
     public List<Course> findAll();
+    
+// lesson02
+    // 新規登録
+    public void insert(Course course);
+    
+    public Course findById(Integer id);
+    // 更新
+    public void update(Course course);
+    
+    // 削除
+    public void deleteById(Integer id);
+    
+    // 複数引数
+    public List<Course> findAll(@Param("courseId") Integer courseId, @Param("courseName") String courseName);
 }
--- src/main/java/com/example/service/CourseService.java
+++ src/main/java/com/example/service/CourseService.java
@@ -10,14 +10,44 @@ import com.example.mapper.CourseMapper;
 
 @Service
 public class CourseService {
-    private final CourseMapper courseMapper;
+	private final CourseMapper courseMapper;
 
-    @Autowired
-    public CourseService(CourseMapper courseMapper) {
-        this.courseMapper = courseMapper;
-    }
+	@Autowired
+	public CourseService(CourseMapper courseMapper) {
+		this.courseMapper = courseMapper;
+	}
 
-    public List<Course> findAll() {
-        return this.courseMapper.findAll();
-    }
+	public List<Course> findAll() {
+		return this.courseMapper.findAll();
+	}
+
+//  lesson02
+	// 新規登録
+	public void insert(String name) { // （入力されたデータ）
+		Course course = new Course();
+		course.setName(name);
+		this.courseMapper.insert(course);
+	}
+	
+	public Course findById(Integer id) {
+		return this.courseMapper.findById(id);
+		}
+		
+	// 更新
+	public void update(Integer id, String name) {
+		Course course = new Course();
+		course.setId(id);
+		course.setName(name);
+		this.courseMapper.update(course);
+	}	
+	
+	// 削除
+	public void deleteById(Integer id) {
+		this.courseMapper.deleteById(id);
+	}
+	
+	// 複数の引数で取得
+	public List<Course> findAll(Integer id, String name) {
+		return this.courseMapper.findAll(id, name);
+	}
 }
